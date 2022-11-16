6c37c5a9e62307743968ce072b3dfb66828e1d7f
Jalen Lamb <2018118881@ub.edu.bz>
2022-11-16 15:46:48 -0600

    CRUD p5

--- cmd/api/toasts.go
+++ cmd/api/toasts.go
@@ -166,3 +166,30 @@ func (app *application) updateToastHandler(w http.ResponseWriter, r *http.Reques
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) deleteToastHandler(w http.ResponseWriter, r *http.Request) {
+	// Get the id for the toast that needs updating
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.notFoundResponse(w, r)
+		return
+	}
+	// Delete the Toast from the database. Send a 404 Not Found status code to the
+	// client if there is no matching record
+	err = app.models.Toasts.Delete(id)
+	// Handle errors
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	// Return 200 Status OK to the client with a success message
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "toast successfully deleted"}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
