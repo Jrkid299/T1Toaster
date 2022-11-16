de8aa1bbaf7595ba5f58b39e59347a7cd502f798
Jalen Lamb <2018118881@ub.edu.bz>
2022-11-16 14:10:06 -0600

    validation 1 - 6

--- internals/data/toasts.go
+++ internals/data/toasts.go
@@ -4,6 +4,8 @@ package data
 
 import (
 	"time"
+
+	"toaster.jalen.net/internals/validator"
 )
 
 type Toast struct {
@@ -18,4 +20,33 @@ type Toast struct {
 	Address   string    `json:"address"`
 	Mode      []string  `json:"mode"`
 	Version   int32     `json:"version"`
+}
+
+func ValidateToast(v *validator.Validator, toast *Toast) {
+	// Use the Check() method to execute our validation checks
+	v.Check(toast.Name != "", "name", "must be provided")
+	v.Check(len(toast.Name) <= 200, "name", "must not be more than 200 bytes long")
+
+	v.Check(toast.Level != "", "level", "must be provided")
+	v.Check(len(toast.Level) <= 200, "level", "must not be more than 200 bytes long")
+
+	v.Check(toast.Contact != "", "contact", "must be provided")
+	v.Check(len(toast.Contact) <= 200, "contact", "must not be more than 200 bytes long")
+
+	v.Check(toast.Phone != "", "phone", "must be provided")
+	v.Check(validator.Matches(toast.Phone, validator.PhoneRX), "phone", "must be a valid phone number")
+
+	v.Check(toast.Email != "", "email", "must be provided")
+	v.Check(validator.Matches(toast.Email, validator.EmailRX), "email", "must be a valid email address")
+
+	v.Check(toast.Website != "", "website", "must be provided")
+	v.Check(validator.ValidWebsite(toast.Website), "website", "must be a valid URL")
+
+	v.Check(toast.Address != "", "address", "must be provided")
+	v.Check(len(toast.Address) <= 500, "address", "must not be more than 500 bytes long")
+
+	v.Check(toast.Mode != nil, "mode", "must be provided")
+	v.Check(len(toast.Mode) >= 1, "mode", "must contain at least 1 entry")
+	v.Check(len(toast.Mode) <= 5, "mode", "must contain at most 5 entries")
+	v.Check(validator.Unique(toast.Mode), "mode", "must not contain duplicate entries")
 }
