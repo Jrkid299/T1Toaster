7ff8508b9a99d7242153f16bf23c89373301b70f
Jalen Lamb <2018118881@ub.edu.bz>
2022-11-16 22:47:40 -0600

    User p4

--- go.mod
+++ go.mod
@@ -9,4 +9,4 @@ require (
 	golang.org/x/time v0.2.0
 )
 
-require golang.org/x/crypto v0.3.0 // indirect
+require golang.org/x/crypto v0.3.0
--- internals/data/users.go
+++ internals/data/users.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
+	"toaster.jalen.net/internals/validator"
 )
 
 type User struct {
@@ -49,4 +50,32 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 		}
 	}
 	return true, nil
+}
+
+// Validate the client request
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address.")
+}
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+	//validate the email
+	ValidateEmail(v, user.Email)
+	//validate the password
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlaintext(v, *user.Password.plaintext)
+	}
+	//Ensure a hash of the password was created
+	if user.Password.hash == nil {
+		panic("missing password hash for the user")
+	}
+
 }
