59ee2e0c741ecf649f9113bf9a32ab3726f9a4aa
Jalen Lamb <2018118881@ub.edu.bz>
2022-11-17 00:13:06 -0600

    Sending E p3 -5 it works

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,7 @@ import (
 	_ "github.com/lib/pq"
 	"toaster.jalen.net/internals/data"
 	"toaster.jalen.net/internals/jsonlog"
+	"toaster.jalen.net/internals/mailer"
 )
 
 // The application version number
@@ -31,6 +32,13 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 // Dependency Injection
@@ -38,6 +46,7 @@ type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
 }
 
 func main() {
@@ -53,6 +62,12 @@ func main() {
 	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
+	// These are flags for the mailer
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "2037666035a398", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "3c2e3bd9549b3a", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Toaster <no-reply@toaster.jalen.net>", "SMTP sender")
 
 	flag.Parse()
 	// Create a logger
@@ -71,6 +86,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 	// Call app.serve() to start the server
 	err = app.serve()
